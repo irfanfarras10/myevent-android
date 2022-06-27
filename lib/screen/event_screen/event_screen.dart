c77ee1aeb8aeb5a25518bcd307b2fe81c42f0a96
Irfan Farras <irfanfarras10@gmail.com>
2022-06-27 22:06:08 +0700

    feat: add tab bar with screen

--- lib/screen/event_screen/event_screen.dart
+++ lib/screen/event_screen/event_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:myevent_android/colors/myevent_color.dart';
 import 'package:myevent_android/route/route_name.dart';
+import 'package:myevent_android/screen/draft_event_screen/draft_event_screen.dart';
 
 class EventScreen extends StatelessWidget {
   const EventScreen({Key? key}) : super(key: key);
@@ -8,8 +10,57 @@ class EventScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Event List Screen'),
+      body: DefaultTabController(
+        length: 5,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, value) {
+            return [
+              SliverAppBar(
+                floating: true,
+                pinned: true,
+                snap: false,
+                forceElevated: true,
+                title: Text(
+                  'Event Anda',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: MyEventColor.secondaryColor,
+                  ),
+                ),
+                bottom: TabBar(
+                  indicatorColor: MyEventColor.secondaryColor,
+                  isScrollable: true,
+                  labelStyle: TextStyle(
+                    fontSize: 16.5,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.bold,
+                  ),
+                  unselectedLabelStyle: TextStyle(
+                    fontSize: 16.5,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.normal,
+                  ),
+                  tabs: [
+                    Tab(text: 'Draft'),
+                    Tab(text: 'Akan Datang'),
+                    Tab(text: 'Sedang Berjalan'),
+                    Tab(text: 'Selesai'),
+                    Tab(text: 'Dibatalkan')
+                  ],
+                ),
+              ),
+            ];
+          },
+          body: TabBarView(
+            children: [
+              DraftEventScreen(),
+              Container(),
+              Container(),
+              Container(),
+              Container(),
+            ],
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Get.toNamed(RouteName.createEventScreen),
