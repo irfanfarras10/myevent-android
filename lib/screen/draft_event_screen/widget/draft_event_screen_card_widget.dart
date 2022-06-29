33f3250b2f2da2896eccf5c818843e150290b6d9
Irfan Farras <irfanfarras10@gmail.com>
2022-06-29 14:32:35 +0700

    fix: text overflow

--- lib/screen/draft_event_screen/widget/draft_event_screen_card_widget.dart
+++ lib/screen/draft_event_screen/widget/draft_event_screen_card_widget.dart
@@ -61,69 +61,78 @@ class DraftEventScreenCardWidget extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Webinar Crypto',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: MyEventColor.secondaryColor,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Webinar Crypto asd ad asd asd asd ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: MyEventColor.secondaryColor,
+                          ),
+                          overflow: TextOverflow.ellipsis,
                         ),
-                      ),
-                      Text(
-                        'Event Organizer',
-                        style: TextStyle(
-                          color: MyEventColor.secondaryColor,
+                        Text(
+                          'Event Organizer asdasdas das ada s d',
+                          style: TextStyle(
+                            color: MyEventColor.secondaryColor,
+                          ),
+                          overflow: TextOverflow.ellipsis,
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Webinar Crypto',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12.0,
-                              color: MyEventColor.secondaryColor,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                'Kamis, 2 Juli 2022',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12.0,
+                                  color: MyEventColor.secondaryColor,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            width: 5.0,
-                          ),
-                          Text(
-                            '•',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: MyEventColor.secondaryColor,
+                            SizedBox(
+                              width: 5.0,
                             ),
-                          ),
-                          SizedBox(
-                            width: 5.0,
-                          ),
-                          Text(
-                            '12.00',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12.0,
-                              color: MyEventColor.secondaryColor,
+                            Text(
+                              '•',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: MyEventColor.secondaryColor,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      Text(
-                        'The Breeze',
-                        style: TextStyle(
-                          color: MyEventColor.secondaryColor,
-                          fontSize: 12.0,
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Text(
+                              '12.00',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12.0,
+                                color: MyEventColor.secondaryColor,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                        Text(
+                          'The Breeze',
+                          style: TextStyle(
+                            color: MyEventColor.secondaryColor,
+                            fontSize: 12.0,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -251,17 +260,18 @@ class DraftEventScreenCardWidget extends StatelessWidget {
                           Padding(
                             padding: const EdgeInsets.all(4.0),
                             child: Icon(
-                              Icons.share,
+                              Icons.people,
                               color: MyEventColor.secondaryColor,
                             ),
                           ),
                           Padding(
                             padding: const EdgeInsets.all(2.0),
                             child: Text(
-                              'Bagikan',
+                              'Peserta',
                               style: TextStyle(
                                 color: MyEventColor.secondaryColor,
                               ),
+                              textAlign: TextAlign.center,
                             ),
                           ),
                         ],
@@ -301,18 +311,17 @@ class DraftEventScreenCardWidget extends StatelessWidget {
                           Padding(
                             padding: const EdgeInsets.all(4.0),
                             child: Icon(
-                              Icons.upload_file,
+                              Icons.share,
                               color: MyEventColor.secondaryColor,
                             ),
                           ),
                           Padding(
                             padding: const EdgeInsets.all(2.0),
                             child: Text(
-                              'Berbagi File',
+                              'Bagikan',
                               style: TextStyle(
                                 color: MyEventColor.secondaryColor,
                               ),
-                              textAlign: TextAlign.center,
                             ),
                           ),
                         ],
