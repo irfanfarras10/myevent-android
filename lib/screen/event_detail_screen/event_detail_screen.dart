6c4fd0f656e2ec884def42249d9066b28bb2fc70
Irfan Farras <irfanfarras10@gmail.com>
2022-07-04 11:58:32 +0700

    feat: add event detail ui data and ticket section

--- lib/screen/event_detail_screen/event_detail_screen.dart
+++ lib/screen/event_detail_screen/event_detail_screen.dart
@@ -20,17 +20,12 @@ class EventDetailScreen extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.edit),
-            tooltip: 'Ubah Data Event',
-          ),
-          IconButton(
-            onPressed: () {},
             icon: Icon(Icons.delete),
             tooltip: 'Hapus Event',
           ),
         ],
       ),
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey.shade200,
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
         child: Column(
@@ -79,7 +74,7 @@ class EventDetailScreen extends StatelessWidget {
                   child: CircularProgressIndicator(),
                 ),
                 errorWidget: (context, url, error) => Container(
-                  color: Colors.black38,
+                  color: Colors.grey.shade100,
                   child: Center(
                     child: Icon(
                       Icons.image,
@@ -88,7 +83,8 @@ class EventDetailScreen extends StatelessWidget {
                 ),
               ),
             ),
-            Padding(
+            Container(
+              color: Colors.white,
               padding: const EdgeInsets.all(15.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -119,6 +115,28 @@ class EventDetailScreen extends StatelessWidget {
                           ),
                         ],
                       ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Text(
+                              'Edit',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Icon(
+                              Icons.edit,
+                              size: 16.5,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
                       Container(
                         padding: EdgeInsets.all(5.0),
                         decoration: BoxDecoration(
@@ -135,10 +153,29 @@ class EventDetailScreen extends StatelessWidget {
                           ),
                         ),
                       ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                          color: MyEventColor.secondaryColor,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(4.0),
+                          ),
+                        ),
+                        child: Text(
+                          'Onsite',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                   SizedBox(
-                    height: 15.0,
+                    height: 20.0,
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -160,7 +197,7 @@ class EventDetailScreen extends StatelessWidget {
                     ],
                   ),
                   SizedBox(
-                    height: 15.0,
+                    height: 20.0,
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -180,7 +217,7 @@ class EventDetailScreen extends StatelessWidget {
                           Icon(
                             Icons.date_range,
                             size: 16.5,
-                            color: Colors.black38,
+                            color: MyEventColor.secondaryColor,
                           ),
                           SizedBox(
                             width: 5.0,
@@ -192,12 +229,275 @@ class EventDetailScreen extends StatelessWidget {
                             ),
                           ),
                         ],
-                      )
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.access_time,
+                            size: 16.5,
+                            color: MyEventColor.secondaryColor,
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(
+                            '09.00 - 15.00',
+                            style: TextStyle(
+                              color: MyEventColor.secondaryColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Lokasi Event',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: MyEventColor.secondaryColor,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5.0,
+                                ),
+                                Text(
+                                  'Jl. Untung Suropati 2 RT 002 / 08   ',
+                                  textAlign: TextAlign.justify,
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: 15.0,
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Row(
+                              children: [
+                                Text(
+                                  'Lihat Lokasi',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Icon(
+                                  Icons.location_on,
+                                  size: 16.5,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   )
                 ],
               ),
             ),
+            SizedBox(
+              height: 15.0,
+            ),
+            Container(
+              color: Colors.white,
+              width: double.infinity,
+              padding: EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Tiket',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Text(
+                              'Edit',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Icon(
+                              Icons.edit,
+                              size: 16.5,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    'Tanggal Registrasi',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: MyEventColor.secondaryColor,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.date_range,
+                        size: 16.5,
+                        color: MyEventColor.secondaryColor,
+                      ),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                      Text(
+                        '18 Juni 2022 - 19 Juni 2022',
+                        style: TextStyle(
+                          color: MyEventColor.secondaryColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 20.0,
+                        height: 10.0,
+                        child: Checkbox(
+                          value: true,
+                          onChanged: (checked) {},
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('Tiket Harian')
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 20.0,
+                        height: 10.0,
+                        child: Checkbox(
+                          value: true,
+                          onChanged: (checked) {},
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('Tiket Berbayar')
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total Tiket (4 Hari)',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '4000',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Reguler',
+                        style: TextStyle(),
+                      ),
+                      Text(
+                        '1000',
+                        style: TextStyle(),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Silver',
+                        style: TextStyle(),
+                      ),
+                      Text(
+                        '1000',
+                        style: TextStyle(),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Gold',
+                        style: TextStyle(),
+                      ),
+                      Text(
+                        '1000',
+                        style: TextStyle(),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Platinum',
+                        style: TextStyle(),
+                      ),
+                      Text(
+                        '1000',
+                        style: TextStyle(),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
